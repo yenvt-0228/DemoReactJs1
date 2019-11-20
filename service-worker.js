3919b68099efc5b4b0821359663e0f4e7d0ca0ec
Yen Vu Thi <vu.thi.yen@sun-asterisk.com>
2019-11-20 16:38:11 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/yenvt-0228/yenvt.github.io/precache-manifest.787210bad22753e7ad4a350826c376d4.js"
+  "/yenvt-0228/DemoReactJs1/precache-manifest.66a4066930c8eef2ef157e31e2720ed6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yenvt-0228/yenvt.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yenvt-0228/DemoReactJs1/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
