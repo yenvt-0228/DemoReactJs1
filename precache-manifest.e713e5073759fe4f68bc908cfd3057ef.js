206bc57ab7cdc396e281b0025b0ae9845d9afb60
Yen Vu Thi <vu.thi.yen@sun-asterisk.com>
2019-11-21 14:49:43 +0700

    Updates

---
+++ precache-manifest.e713e5073759fe4f68bc908cfd3057ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a562ad471ee84329f728ba7e13bc74ea",
+    "url": "./index.html"
+  },
+  {
+    "revision": "84fb7273f7cf186c79a9",
+    "url": "./static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a2fa544d1dbdd8f765b3",
+    "url": "./static/js/2.e4e2ba8c.chunk.js"
+  },
+  {
+    "revision": "84fb7273f7cf186c79a9",
+    "url": "./static/js/main.67dc2aa2.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/DemoReactJs1.git/precache-manifest.6202d29654ae0674efc9cde772f2b580.js"
+  "./precache-manifest.e713e5073759fe4f68bc908cfd3057ef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DemoReactJs1.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
